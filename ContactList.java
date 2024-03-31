2ba3707dc49e11f87035438caef1a9c88e8cc421
HemaShaswar <96527414+HemaShaswar@users.noreply.github.com>
2024-03-31 10:40:18 +0000

    modify contact and delecte contact done

--- ContactList.java
+++ ContactList.java
@@ -1,58 +1,96 @@
 public class ContactList { //Singley Linked List
-    public static Contact head;
-    public static Contact tail;
-    public static int size;
+    public Contact head;
+    public Contact tail;
+    public int size;
 
     public ContactList(){
-        head = null;
-        tail = null;
-        size = 0;
-    }
+        this.head = null;
+        this.tail = null;
+        this.size = 0;
+    };
 
     //adds new contact to the end of the list if name is not duplicated
-    public static void addContact(Contact newContact){
-        if (size == 0) {
-            head = newContact;
-            System.out.println("Contact Added Successfully!");
-            size++;
+    public void addContact(Contact newContact){
+        if (this.size == 0) {
+            this.head = newContact;
+            System.out.println("Contact Added Successfully to the start!");
+            this.size++;
             return;
         } 
         if (findContact(newContact.name) != null) {
-            tail.next = newContact;
-            tail = newContact;
+            this.tail.next = newContact;
+            this.tail = newContact;
             System.out.println("Contact Added Successfully!");
-            size++;
+            this.size++;
             return;
         }
         System.out.println("Contact Name Already exists. Please choose a different name");
-    }
+    };
 
-    public static void deleteContact(String deleteName){
-        if (findContact(deleteName) != null){
-
-            size--;
+    public void deleteContact(String deleteName){
+        Contact deleteContact = findContact(deleteName);
+        if (deleteContact != null){
+            if (deleteContact.next == null) {
+                deleteContact.prev.next = null;
+                this.tail = deleteContact.prev;
+                deleteContact.prev = null;
+                this.size--;
+                return;
+            }
+            if (deleteContact.prev == null) {
+                deleteContact.next.prev = null;
+                this.head = deleteContact.next;
+                deleteContact.next = null;
+                this.size--;
+                return;
+            }
+            deleteContact.prev.next = deleteContact.next;
+            deleteContact.next.prev = deleteContact.prev;
+            deleteContact.next = deleteContact.prev = null;
+            this.size--;
             return;
-        }
+        };
 
-        System.out.println("Contact Name Does Not Exist");
+    };
+
+    public void callContact(String callName){
+        Contact callContact = findContact(callName);
+
+        if (callContact != null) {
+            System.out.printf("Calling %s on the number: %s...",callContact.name,callContact.number);
+        };
+    };
+
+    public void modifyContactName(String oldContactName,String newName){
+        Contact oldContact = findContact(newName);
+        if (oldContact != null) {
+            oldContact.number = newName;
+        };
     }
 
+    public void modifyContactNumber(String oldContactName, String newNumber){
+        Contact oldContact = findContact(oldContactName);
+        if (oldContact != null) {
+            oldContact.number = newNumber;
+        };
+    }
 
     //searches for contact by name
     //returns searched contact or null if not found 
-    public static Contact findContact(String searchName){
-        Contact currentContact = head;
+    public Contact findContact(String findName){
+        Contact currentContact = this.head;
 
-        for (int i = 0; i < size-1; i++) {
+        for (int i = 0; i < size; i++) {
 
-            if (currentContact.name == searchName) {
+            if (currentContact.name == findName) {
                 return currentContact;
-            }
+            };
 
             currentContact = currentContact.next;
 
-        }
+        };
 
+        System.out.println("Provided Name Does Not Exist in the Contact List");
         return null;
-    }
+    };
 }

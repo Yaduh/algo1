98115e0f4b99030af6eec7a31e6c8ef30f9da12f
HemaShaswar <96527414+HemaShaswar@users.noreply.github.com>
2024-03-31 15:07:37 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,8 +2,20 @@ public class Main {
     public static void main(String... args) {
         System.out.println("Welcome to the Phone Book App\n");
         ContactList ContactL = new ContactList();
-        Contact newContact = new Contact("Hema", "0770121314");
-        ContactL.addContact(newContact);
-        ContactL.callContact("Hema");
+        
+        ContactL.addContact("Hema", "0770121314");
+        ContactL.addContact("Zebra","09409");
+        ContactL.addContact("Lion","2394803284");
+        ContactL.addContact("Tiger","234234234");
+        ContactL.addContact("Elephant","23423423");
+        ContactL.addContact("Giraffe","23423423");
+
+        System.out.println("Before sorting:");
+        ContactL.displayContactList();
+
+        ContactL.sort();
+
+        System.out.println("\nAfter sorting:");
+        ContactL.displayContactList();
     }
 }
